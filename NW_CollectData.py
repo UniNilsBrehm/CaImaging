2fdd14b113aae0ee1a18e9d3792b24c9ac651d40
UniNilsBrehm <hd280kilxtc>
2022-10-23 15:00:59 +0200

    changes on collecting data

--- NW_CollectData.py
+++ NW_CollectData.py
@@ -21,46 +21,73 @@ def import_f_raw(file_dir):
 base_dir = uf.select_dir()
 rec_dob = os.path.split(base_dir)[1][:-4]
 # Ignore all files and only take directories
-rec_list = [s for s in os.listdir(base_dir) if '.' not in s]
+dob_count = len([s for s in os.listdir(base_dir) if '.' not in s])
 
-uf.msg_box(f'DOB: {rec_dob}', 'COLLECTING DATA ... PLEASE WAIT ...', '+')
+all_dirs = []
+for root, dirs, files in os.walk(base_dir, topdown=True):
+    for name in dirs:
+        # print(os.path.join(root, name))
+        all_dirs.append(os.path.join(root, name))
+
+rec_list = [s for s in all_dirs if 'refs' not in s]
+rec_list = rec_list[dob_count:]
+
+uf.msg_box(f'INFO', 'COLLECTING DATA ... PLEASE WAIT ...', '+')
 
 # Cutout Settings in secs
 pad_before = 5
 pad_after = 20
 
+count = 0
 t0 = time.time()
 data_frame = pd.DataFrame()
 anatomy = dict.fromkeys(rec_list)
-for rec_name in rec_list:
-    anatomy[rec_name] = pd.read_csv(f'{base_dir}/{rec_name}/{rec_name}_anatomy.csv')
+for rec_dir in rec_list:
+    count += 1
+    rec_name = os.path.split(rec_dir)[1]
+    anatomy[rec_name] = pd.read_csv(f'{rec_dir}/{rec_name}_anatomy.csv')
+    # Ignore this recording if there are no good cells (no entries in anatomy.csv)
     if anatomy[rec_name].shape[0] > 0:
         # get raw values of this recording
-        f_raw = import_f_raw(f'{base_dir}/{rec_name}/{rec_name}_raw.txt')
+        f_raw = import_f_raw(f'{rec_dir}/{rec_name}_raw.txt')
+        # Get only the good cells (based on LM score)
         f_raw_selected = f_raw[anatomy[rec_name].keys()]
-        f_raw_selected_filtered = uf.filter_raw_data(sig=f_raw_selected, win=13, o=2)
+
+        # Filter Raw Data
+        f_raw_selected_filtered = uf.filter_raw_data(sig=f_raw_selected, win=7, o=3)
+
+        # Compute f base line (5th percentile of all values)
         fbs = np.percentile(f_raw_selected_filtered, 5, axis=0)
+
+        # Compute delta f over f
         df_f = (f_raw_selected_filtered - fbs) / fbs
+
+        # Compute z-scores
+        z_scores = (df_f - np.mean(df_f, axis=0)) / np.std(df_f, axis=0)
+
         # get stimulus
-        stimulus = uf.import_txt_stimulation_file(f'{base_dir}/{rec_name}/', f'{rec_name}_stimulation', float_dec='.')
+        stimulus = uf.import_txt_stimulation_file(f'{rec_dir}/', f'{rec_name}_stimulation', float_dec='.')
         fr_rec = uf.estimate_sampling_rate(f_raw_selected, f_stimulation=stimulus, print_msg=False)
         time_axis = uf.convert_samples_to_time(f_raw_selected, fr=fr_rec)
 
         # get protocol
-        protocol = pd.read_csv(f'{base_dir}/{rec_name}/{rec_name}_protocol.csv')
+        protocol = pd.read_csv(f'{rec_dir}/{rec_name}_protocol.csv')
 
         # get linear regression model results
-        lm = np.load(f'{base_dir}/{rec_name}/{rec_name}_lm_results.npy', allow_pickle=True).item()
+        lm = np.load(f'{rec_dir}/{rec_name}_lm_results.npy', allow_pickle=True).item()
 
         # Compute Calcium Impulse Response Function (CIRF)
         cirf_tau = 10
         cirf = uf.create_cif(fr_rec, tau=cirf_tau)
-        # Compute Regressor for the entire stimulus trace for plotting
+
+        # Compute Regressor for the entire stimulus trace
         binary, reg, _, _ = uf.create_binary_trace(
             sig=f_raw, cirf=cirf, start=protocol['Onset_Time'], end=protocol['Offset_Time'],
             fr=fr_rec, ca_delay=0, pad_before=5, pad_after=20, low=0, high=1
         )
+        # Normalize Regressor to max = 1
         reg_norm = reg / np.max(reg)
+        # Correct for too long Regressors (due to convolution ...)
         reg_norm = reg_norm[:len(binary)]
 
         # Get List of used stimuli
@@ -85,7 +112,6 @@ for rec_name in rec_list:
         mean_scores_rois = dict.fromkeys(f_raw_selected, ['NaN'] * len(f_raw_selected))
         for k in range(protocol.shape[0]):
             onset = int(protocol['Onset_Time'].iloc[k] * fr_rec)
-            # offset = onset + int(protocol['Duration'].iloc[k]/1000 * fr_rec)
             offset = int(protocol['Offset_Time'].iloc[k] * fr_rec)
 
             # Set cutout window
@@ -135,6 +161,7 @@ for rec_name in rec_list:
             roi_data['raw'] = f_raw_selected[roi]
             roi_data['raw_filtered'] = f_raw_selected_filtered[roi]
             roi_data['df'] = df_f[roi]
+            roi_data['z-score'] = z_scores[roi]
             roi_data['fbs'] = fbs[k]
             roi_data['cirf_tau'] = cirf_tau
             roi_data['anatomy'] = [anatomy[rec_name][roi].item()] * size
@@ -155,11 +182,12 @@ for rec_name in rec_list:
             roi_data['sample'] = np.linspace(1, size, size).astype('int')
 
             data_frame = pd.concat([data_frame, roi_data])
+        print(f'{count} / {len(rec_list)} Done')
 t1 = time.time()
 uf.msg_box('INFO', f'Collecting Data took: {np.round(t1-t0, 2)} secs \n'
                    f'Data Frame Size: {len(data_frame):,}', '+')
 
 # Store Data Frame as csv file to HDD
-data_frame.to_csv(f'{base_dir}/data_frame.csv')
+data_frame.to_csv(f'{base_dir}/data_frame_complete.csv')
 t2 = time.time()
 uf.msg_box('INFO', f'Storing Data Frame to HDD took: {np.round(t2-t1, 2)} secs', '+')
--- NW_Final_Analysis.py
+++ NW_Final_Analysis.py
@@ -1,6 +1,7 @@
 import analysis_util_functions as uf
 import pandas as pd
 import numpy as np
+import os
 import matplotlib.pyplot as plt
 from IPython import embed
 
@@ -9,30 +10,51 @@ df = pd.read_csv(file_dir, index_col=0)
 
 embed()
 exit()
+time_before = 5
+time_after = 20
 
-# Collect responses to step 100
-idx_step_100 = (df['stimulus_type'] == 'Step') \
-               & (df['stimulus_parameter'] == 100) \
-               & (df['anatomy'] == 'tg') \
-               & (df['mean_score'] > 0)
+idx = (df['stimulus_onset_type'] == 'Step') \
+      & (df['stimulus_parameter'] == 100) \
+      & (df['anatomy'] == 'tg') \
+      & (df['mean_score'] > 0.2)
 
-data = df[idx_step_100].copy()
-cell_names = data['id'].unique()
-a = []
-for c in cell_names:
-    d = data[data['id'] == c].copy()
-    for tr in d['trial'].unique():
-        idx = d['trial'] == tr
-        size = len(d.loc[(idx, 'df')])
-        f = d.loc[(idx, 'df')].to_numpy()
-        z = (f - np.mean(f))/np.std(f)
-        a.append(z)
+idx_ramp = np.where(idx == True)[0]
+cell_names = df.loc[idx]['id'].unique()
+z_scores = [[]] * len(idx_ramp)
+fr = df.iloc[idx_ramp[0]]['fr']
+for k, s in enumerate(idx_ramp):
+    before = int(time_before * fr)
+    after = int(time_after * fr)
+    z = df.iloc[s-before:s+after]['z-score']
+    z_scores[k] = z
 
 plt.figure()
 plt.title(f'n={cell_names.shape[0]}')
-m = np.mean(a, axis=0)
-sem = np.std(a, axis=0) / np.sqrt(len(m))
-plt.plot(m, 'k')
-plt.plot(m-sem, 'r')
-plt.plot(m+sem, 'r')
+m = np.mean(z_scores, axis=0)
+sem = np.std(z_scores, axis=0) / np.sqrt(len(m))
+t_axis = uf.convert_samples_to_time(sig=m, fr=fr)
+plt.plot(t_axis, m, 'k')
+plt.plot(t_axis, m-sem, 'r')
+plt.plot(t_axis, m+sem, 'r')
 plt.show()
+
+exit()
+
+# # Compute z scores
+# cells_unique = df['id'].unique()
+# for cell in cells_unique:
+#     # Get Raw Values
+#     r = df.loc[(df['id'] == cell, 'raw')]
+#     # Filter Raw Values
+#     r_fil = uf.savitzky_golay(y=r.to_numpy(), window_size=5, order=2)
+#     r_fil = pd.DataFrame(r_fil)
+#     # Compute delta f over f
+#     f_df, f_rois, fbs = uf.compute_df_over_f(f_values=r_fil, window_size=100, per=5, fast=True)
+#     # fbs = np.round(np.percentile(r_fil, 5, axis=0), 3)
+#     # f_df = (r_fil - fbs) / fbs
+#     # Compute z score
+#     z = (f_df-np.mean(f_df, axis=0)) / np.std(f_df, axis=0)
+#     df.loc[(df['id'] == cell, 'z_score')] = z.to_numpy()
+
+# save_dir = os.path.split(file_dir)[0]
+# df.to_csv(f'{save_dir}/data_frame2.csv')
